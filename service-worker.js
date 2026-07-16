f9270bcdb9f3a3ec655a97ff27bd108064048456
Sam Van Royen <studiosampersand@gmail.com>
2026-07-16 23:25:35 +0200

    huisstijl toegepast

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='mindmark-v0.000.009';
+const CACHE='mindmark-v0.000.011';
 const CORE=['./','./index.html','./app/','./app/index.html','./faq/','./faq/index.html','./privacy-legal/','./privacy-legal/index.html','./contact/','./contact/index.html','./assets/styles.css','./assets/logo.svg','./assets/icons.svg','./assets/icon-192.svg','./assets/icon-512.svg','./js/app.js','./data/trackers.js','./manifest.webmanifest'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
